ca0becaa7441993f2dff528f7fbb8d82d75f5285
Joe Zhou <zhoushuqun@gmail.com>
2014-06-15 18:57:54 +0800

    Add unit test for parsing userinfo.json.

---
+++ src/test/java/com/redv/okcoin/domain/UserInfoTest.java
@@ -0,0 +1,46 @@
+package com.redv.okcoin.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UserInfoTest {
+
+	@Test
+	public void test() throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		UserInfo userInfo = mapper.readValue(getClass().getResource("userinfo.json"),
+				UserInfo.class);
+
+		assertTrue(userInfo.isResult());
+
+		assertEquals(new BigDecimal("1000"), userInfo.getInfo().getFunds().getFree().get("cny"));
+		assertEquals(new BigDecimal("10"), userInfo.getInfo().getFunds().getFree().get("btc"));
+		assertEquals(new BigDecimal("0"), userInfo.getInfo().getFunds().getFree().get("ltc"));
+
+		assertEquals(new BigDecimal("1000"), userInfo.getInfo().getFunds().getFreezed().get("cny"));
+		assertEquals(new BigDecimal("10"), userInfo.getInfo().getFunds().getFreezed().get("btc"));
+		assertEquals(new BigDecimal("0"), userInfo.getInfo().getFunds().getFreezed().get("ltc"));
+	}
+
+	@Test
+	public void testError() throws JsonParseException, JsonMappingException,
+			IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		UserInfo userInfo = mapper.readValue(
+				getClass().getResource("userinfo-error.json"), UserInfo.class);
+
+		assertFalse(userInfo.isResult());
+		assertEquals(10000, userInfo.getErrorCode());
+	}
+
+}
