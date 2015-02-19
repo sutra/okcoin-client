64cbb352ae14363ecc192cb551742164e5af9728
Joe Zhou <zhoushuqun@gmail.com>
2015-02-19 12:27:58 +0800

    Add test for market orders.

--- okcoin-client-rest/src/test/java/org/oxerr/okcoin/rest/dto/OrderHistoryTest.java
+++ okcoin-client-rest/src/test/java/org/oxerr/okcoin/rest/dto/OrderHistoryTest.java
@@ -29,4 +29,36 @@ public class OrderHistoryTest extends UnmarshalTest {
 		assertEquals(Type.SELL, order.getType());
 	}
 
+	@Test
+	public void testOrderHistoryWithMarketOrders() throws IOException {
+		OrderHistory history = readValue("order_history_with_market_orders.json", OrderHistory.class);
+		assertEquals(0, history.getCurrentPage());
+		assertEquals(10, history.getPageLength());
+		assertEquals(4319, history.getTotal());
+
+		// buy market
+		Order order = history.getOrders()[1];
+		assertEquals(new BigDecimal("0"), order.getAmount());
+		assertEquals(new BigDecimal("1467.99"), order.getAvgPrice());
+		assertEquals(1424315585000L, order.getCreateDate().toEpochMilli());
+		assertEquals(new BigDecimal("0.01"), order.getDealAmount());
+		assertEquals(342113073L, order.getOrderId());
+		assertEquals(new BigDecimal("15"), order.getPrice());
+		assertEquals(Status.FULLY_FILLED, order.getStatus());
+		assertEquals("btc_cny", order.getSymbol());
+		assertEquals(Type.BUY_MARKET, order.getType());
+
+		// sell market
+		order = history.getOrders()[2];
+		assertEquals(new BigDecimal("0.01"), order.getAmount());
+		assertEquals(new BigDecimal("1467.98"), order.getAvgPrice());
+		assertEquals(1424315429000L, order.getCreateDate().toEpochMilli());
+		assertEquals(new BigDecimal("0.01"), order.getDealAmount());
+		assertEquals(342110762L, order.getOrderId());
+		assertEquals(new BigDecimal("0"), order.getPrice());
+		assertEquals(Status.FULLY_FILLED, order.getStatus());
+		assertEquals("btc_cny", order.getSymbol());
+		assertEquals(Type.SELL_MARKET, order.getType());
+	}
+
 }
